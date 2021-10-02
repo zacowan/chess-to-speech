bb1317694fed651f15be6ccd18eb43289142dc00
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2021-10-02 15:31:23 -0400

    feat(app.py): swapped to Dialogflow ES

--- flask_api/src/app.py
+++ flask_api/src/app.py
@@ -1,9 +1,7 @@
 import uuid
 
 from flask import Flask, request
-from google.cloud.dialogflowcx_v3beta1.services.agents import AgentsClient
-from google.cloud.dialogflowcx_v3beta1.services.sessions import SessionsClient
-from google.cloud.dialogflowcx_v3beta1.types import session
+from google.cloud import dialogflow
 
 app = Flask(__name__)
 
@@ -20,47 +18,34 @@ def detect_intent():
 
 @app.route("/test-detect-intent")
 def test_detect_intent():
-    # TODO(developer): Replace these values when running the function
     project_id = "chess-master-andy-mhyo"
-    # For more information about regionalization see https://cloud.google.com/dialogflow/cx/docs/how/region
-    location_id = "us-east1"
-    # For more info on agents see https://cloud.google.com/dialogflow/cx/docs/concept/agent
-    agent_id = "953810bf-ace9-40a4-a9a0-dff09d089003"
-    agent = f"projects/{project_id}/locations/{location_id}/agents/{agent_id}"
-    # For more information on sessions see https://cloud.google.com/dialogflow/cx/docs/concept/session
     session_id = uuid.uuid4()
-    texts = ["Hello"]
-    # For more supported languages see https://cloud.google.com/dialogflow/es/docs/reference/language
+    texts = ["Hey Andy, let's play a game."]
     language_code = "en-us"
-    return detect_intent_texts(agent, session_id, texts, language_code)
+    return detect_intent_texts(project_id, session_id, texts, language_code)
 
-def detect_intent_texts(agent, session_id, texts, language_code):
-    """Returns the result of detect intent with texts as inputs.
-    Using the same `session_id` between requests allows continuation
-    of the conversation."""
-    session_path = f"{agent}/sessions/{session_id}"
-    print(f"Session path: {session_path}\n")
-    client_options = None
-    agent_components = AgentsClient.parse_agent_path(agent)
-    location_id = agent_components["location"]
-    if location_id != "global":
-        api_endpoint = f"{location_id}-dialogflow.googleapis.com:443"
-        print(f"API Endpoint: {api_endpoint}\n")
-        client_options = {"api_endpoint": api_endpoint}
-    session_client = SessionsClient(client_options=client_options)
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
 
     for text in texts:
-        text_input = session.TextInput(text=text)
-        query_input = session.QueryInput(text=text_input, language_code=language_code)
-        request = session.DetectIntentRequest(
-            session=session_path, query_input=query_input
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
         )
-        response = session_client.detect_intent(request=request)
 
         print("=" * 20)
-        print(f"Query text: {response.query_result.text}")
-        response_messages = [
-            " ".join(msg.text.text) for msg in response.query_result.response_messages
-        ]
-        print(f"Response text: {' '.join(response_messages)}\n")
-        return f"Response text: {' '.join(response_messages)}\n"
+        print("Query text: {}".format(response.query_result.query_text))
+        print(
+            "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+            )
+        )
+        print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+        return "Fulfillment text: {}\n".format(response.query_result.fulfillment_text)
