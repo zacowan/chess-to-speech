4091e0715dee3805110f3651c272a49172eecc1d
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2021-10-19 19:13:27 -0400

    refactor(aaron): refactored files
    Updated README with my instructions
    Moved images into their own directory
    Created directory for audio files

--- chess_client/audioEngine.py
+++ chess_client/audioEngine.py
@@ -11,29 +11,35 @@ import wave
 import theMain
 import restService
 
-THRESHOLD = 1800 # Quiteness threshold adjust this to adjust min volume required for End Point Detection
+# Audio path
+AUDIO_PATH = "./user_audio"
+
+# Quiteness threshold adjust this to adjust min volume required for End Point Detection
+THRESHOLD = 1800
 CHUNK_SIZE = 1024
 FORMAT = pyaudio.paInt16
 RATE = 44100
 
-#Future FEATURE: While the User is speaking Andy stops talking or goes low volume
+# Future FEATURE: While the User is speaking Andy stops talking or goes low volume
+
 
 def setupAudioEngine():
     count = 0
     while not theMain.isClosed():
-        file_path= "UserSpeech" + str(count)+".wav"
+        file_path = f"{AUDIO_PATH}/UserSpeech{str(count)}.wav"
         record_to_file(file_path)
-        restService.sendUserAudio(file_path) #send to Andy Api
-        
-        #This is to create a buffer of .wav files, so that they aren't deleted immediately
-        count+=1
-        if count >=10:
-            count=0
-        
-        
+        restService.sendUserAudio(file_path)  # send to Andy Api
+
+        # This is to create a buffer of .wav files, so that they aren't deleted immediately
+        count += 1
+        if count >= 10:
+            count = 0
+
+
 def is_silent(snd_data):
     "Returns 'True' if below the 'silent' threshold"
     return max(snd_data) < THRESHOLD
+
 
 def normalize(snd_data):
     "Average the volume out"
@@ -45,14 +51,15 @@ def normalize(snd_data):
         r.append(int(i*times))
     return r
 
+
 def trim(snd_data):
     "Trim the blank spots at the start and end"
-    def _trim(snd_data, reverse): #reverse is a decrease to threshold as words can end quite which would cause them to get cut off for not meeting threshold
+    def _trim(snd_data, reverse):  # reverse is a decrease to threshold as words can end quite which would cause them to get cut off for not meeting threshold
         snd_started = False
         r = array('h')
 
         for i in snd_data:
-            if not snd_started and abs(i)>THRESHOLD-reverse:
+            if not snd_started and abs(i) > THRESHOLD-reverse:
                 snd_started = True
                 r.append(i)
 
@@ -61,13 +68,14 @@ def trim(snd_data):
         return r
 
     # Trim to the left
-    snd_data = _trim(snd_data,0)
+    snd_data = _trim(snd_data, 0)
 
     # Trim to the right
     snd_data.reverse()
-    snd_data = _trim(snd_data,600)
+    snd_data = _trim(snd_data, 600)
     snd_data.reverse()
     return snd_data
+
 
 def add_silence(snd_data, seconds):
     "Add silence to the start and end of 'snd_data' of length 'seconds' (float)"
@@ -77,20 +85,21 @@ def add_silence(snd_data, seconds):
     r.extend(silence)
     return r
 
+
 def record():
     """
-    Record a word or words from the microphone and 
+    Record a word or words from the microphone and
     return the data as an array of signed shorts.
 
-    Normalizes the audio, trims silence from the 
-    start and end, and pads with 0.5 seconds of 
-    blank sound to make sure VLC et al can play 
+    Normalizes the audio, trims silence from the
+    start and end, and pads with 0.5 seconds of
+    blank sound to make sure VLC et al can play
     it without getting chopped off.
     """
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT, channels=1, rate=RATE,
-        input=True, output=True,
-        frames_per_buffer=CHUNK_SIZE)
+                    input=True, output=True,
+                    frames_per_buffer=CHUNK_SIZE)
 
     num_silent = 0
     num_len = 0
@@ -107,29 +116,32 @@ def record():
 
         silent = is_silent(snd_data)
 
-        if silent and snd_started: #Increments silent counter
+        if silent and snd_started:  # Increments silent counter
             num_silent += 1
-            num_len+=1
-        elif not silent and snd_started: # resets silent counter
+            num_len += 1
+        elif not silent and snd_started:  # resets silent counter
             num_silent = 0
-            num_len+=1
-        elif not silent and not snd_started:# if it detects audio for the first time
+            num_len += 1
+        elif not silent and not snd_started:  # if it detects audio for the first time
             snd_started = True
 
-        if snd_started and (num_silent > 60 or num_len >700): #Ending conditions Silent for more than 50 iterations and total audio grater than 700 iterations
+        # Ending conditions Silent for more than 50 iterations and total audio grater than 700 iterations
+        if snd_started and (num_silent > 60 or num_len > 700):
             break
 
     sample_width = p.get_sample_size(FORMAT)
     stream.stop_stream()
     stream.close()
     p.terminate()
-    #del r[0:8000] # not working hot fix, left here for future experiments
-    r = normalize(r)  #Normalizes audio - "Average the volume out"
-    r = trim(r) #trims the silence from the start and end
-    r = add_silence(r, 0.5) #adds .5 seconds of silence to the start and end
+    # del r[0:8000] # not working hot fix, left here for future experiments
+    r = normalize(r)  # Normalizes audio - "Average the volume out"
+    r = trim(r)  # trims the silence from the start and end
+    r = add_silence(r, 0.5)  # adds .5 seconds of silence to the start and end
     return sample_width, r
 
-#Generate a .wav file based on user speech (end point detection included)
+# Generate a .wav file based on user speech (end point detection included)
+
+
 def record_to_file(path):
     "Records from the microphone and outputs the resulting data to 'path'"
     sample_width, data = record()
@@ -142,36 +154,36 @@ def record_to_file(path):
     wf.writeframes(data)
     wf.close()
 
+
 def playAudio(filepath):
-    #define stream chunk   
-    chunk = 1024  
-    
-    #open a wav format music  
-    f = wave.open(filepath,"rb")  
-    #instantiate PyAudio  
-    p = pyaudio.PyAudio()  
-    #open stream  
-    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
-                    channels = f.getnchannels(),  
-                    rate = f.getframerate(),  
-                    output = True)  
-    #read data  
-    data = f.readframes(chunk)  
-    
-    #play stream  
-    while data and not theMain.isClosed():  
-        stream.write(data)  
-        data = f.readframes(chunk)  
-    
-    #stop stream  
-    stream.stop_stream()  
-    stream.close()  
-    
-    #close PyAudio  
+    # define stream chunk
+    chunk = 1024
+
+    # open a wav format music
+    f = wave.open(filepath, "rb")
+    # instantiate PyAudio
+    p = pyaudio.PyAudio()
+    # open stream
+    stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
+                    channels=f.getnchannels(),
+                    rate=f.getframerate(),
+                    output=True)
+    # read data
+    data = f.readframes(chunk)
+
+    # play stream
+    while data and not theMain.isClosed():
+        stream.write(data)
+        data = f.readframes(chunk)
+
+    # stop stream
+    stream.stop_stream()
+    stream.close()
+
+    # close PyAudio
     p.terminate()
-    
-    
-    
-#For testing purposes
-#if __name__ == '__main__':
+
+
+# For testing purposes
+# if __name__ == '__main__':
 #    playAudio('UserSpeech0.wav')
--- chess_client/theMain.py
+++ chess_client/theMain.py
@@ -9,25 +9,30 @@ import gameEngine
 import audioEngine
 import restService
 
-init_complete = False #May not need, dependent on Rest API testing, For now I will leave this here
-close = False #If this gets set to true it will notify all threads to close and cause the program to end
+# May not need, dependent on Rest API testing, For now I will leave this here
+init_complete = False
+close = False  # If this gets set to true it will notify all threads to close and cause the program to end
 
-#Method for Threads to see if they need to close
+# Method for Threads to see if they need to close
+
+
 def isClosed():
     return close
+
 
 def main():
     print("Window being made.")
     screen = window.startScreen()  # Create Window
     print("Window Completed.")
     print("Audio Thread Being made.")
-    audio = threading.Thread(target= audioEngine.setupAudioEngine)  # @IgnorePep8
+    audio = threading.Thread(
+        target=audioEngine.setupAudioEngine)  # @IgnorePep8
     print("Audio Thread Completed.")
     print("Audio Thread Starting.")
     audio.start()  # Thread that Handles Incoming and Outgoing audio
     print("Audio Thread Running.")
     print("Rest Service Thread Being made.")
-    rest = threading.Thread(target= restService.setupRestService)  # @IgnorePep8
+    rest = threading.Thread(target=restService.setupRestService)  # @IgnorePep8
     print("Rest Service Thread Completed.")
     print("Rest Service Thread Starting.")
     rest.start()  # Thread that Handles Incoming and Outgoing API Data
@@ -38,5 +43,3 @@ def main():
 
 if __name__ == '__main__':  # @IgnorePep8
     main()
-
-
