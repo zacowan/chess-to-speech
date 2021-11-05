6516bb7a7070b39a379b644c3904ab550a93f590
Nate-Stull <nt.copf@gmail.com>
2021-11-05 17:45:35 -0400

    fixed run_dev scripts

--- andy_api/run_dev_demo.sh
+++ andy_api/run_dev_demo.sh
@@ -4,7 +4,7 @@
 # # For Bricson:
 # ABSOLUTE_PATH_TO_KEY="C:\Users\selen\Desktop\ChessToSpeech\chess-to-speech\andy_api\service_key_file.json"
 # For Zach:
-ABSOLUTE_PATH_TO_KEY="/home/natestull/Documents/School/CIS4930 - Special Topics in CISE: Spoken Dialogue Systems/Project/develop/chess-to-speech/andy_api/api/service_key_file.json"
+ABSOLUTE_PATH_TO_KEY=""
 
 # Set path to service account key
 export GOOGLE_APPLICATION_CREDENTIALS=$ABSOLUTE_PATH_TO_KEY
