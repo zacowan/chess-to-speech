374c29fbcd745550433c293c4ea6fbff7f0584d1
Nate-Stull <nt.copf@gmail.com>
2021-11-05 18:38:09 -0400

    Added template basic code to set up best_move intent, which would allow a player to ask andy for their best move.

---
+++ andy_api/api/intent_processing/best_move.py
@@ -0,0 +1,51 @@
+"""
+TODO add info about intent
+"""
+
+from .utils import get_random_choice
+# TODO add api state manager variables for this interaction?
+
+import chess
+
+import os
+
+
+HAPPY_PATH_RESPONSES = [
+    "Lemme give ya a hand...Move {from_location} to {to_location}."
+    "Okay! I know I'm pretty good, so here's a tip- move {from_location} to {to_location}."
+]
+
+# TODO add error responses?
+
+
+def get_engine():
+    dirname = os.path.dirname(__file__)
+    engine_filename = dirname + "../UCI_engine/stockfish"
+    engine = chess.engine.SimpleEngine.popen_uci(engine_filename) #load stockfish as chess engine
+    return engine
+
+def get_best_move(board_str):
+    engine = get_engine()
+    board = chess.Board(board_str)
+    bestMove = engine.play(board, chess.engine.Limit(time=0.1)).move
+    engine.close()
+    return bestMove
+
+def handle(session_id, intent_model, board_str):
+    """TODO add details about method
+
+    """
+    if intent_model.all_required_params_present is True:
+        static_choice = get_random_choice(HAPPY_PATH_RESPONSES)
+        best_move = get_best_move(board_str)
+
+        from_location = move[0:2]
+        to_location = move[2:4]
+
+        return static_choice.format(
+            from_location=from_location,
+            to_location = to_location
+        ), True, board_str
+
+        # TODO add error messages/check for errors from player?
+
