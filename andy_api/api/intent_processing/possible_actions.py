957f0804a7ea52005bf75848eb38d0d15badfc77
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2021-11-06 13:56:50 -0400

    fix: fixed errors

--- andy_api/api/intent_processing/possible_actions.py
+++ andy_api/api/intent_processing/possible_actions.py
@@ -14,14 +14,14 @@ HAPPY_PATH_RESPONSES = [
 ]
 
 GAME_STARTED_RESPONSES = [
-    "You can make a move, ask legal moves for a piece, and ask what your best move is!",
+    "You can tell me a piece to move, ask legal moves for a piece, and ask what your best move is!",
 ]
 
 
-def handle(intent_model):
+def handle():
     if (state_manager.get_game_state["game_started"] == True):
         return get_random_choice(GAME_STARTED_RESPONSES)
-    
+
     return get_random_choice(HAPPY_PATH_RESPONSES)
-##If a game has started: tell user they can make a move (if their turn), 2. ask legal moves for a piece, 3. ask what best move is
-#api folder. statemanager.pi ask if game started yet
+# If a game has started: tell user they can make a move (if their turn), 2. ask legal moves for a piece, 3. ask what best move is
+# api folder. statemanager.pi ask if game started yet
