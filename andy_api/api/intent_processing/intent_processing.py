35191a948d7cd7aec76d756e4cd236201cf38060
Nathaniel Stull <47871658+Nate-Stull@users.noreply.github.com>
2021-11-06 21:34:21 -0400

    added elif for best_move intent

--- andy_api/api/intent_processing/intent_processing.py
+++ andy_api/api/intent_processing/intent_processing.py
@@ -32,7 +32,7 @@ Example Intent Model:
 """
 from api.state_manager import get_game_state
 from .utils import INTENT_MAPPING, RESPONSE_TYPES, get_random_choice
-from . import choose_side, move_piece_from, move_piece_to, wake_up_phrase
+from . import choose_side, move_piece_from, move_piece_to, best_move, wake_up_phrase
 
 
 STATIC_RESPONSES = {
@@ -108,6 +108,9 @@ def fulfill_intent(session_id, board_str, intent_data):
                 session_id, intent_data, board_str)
         elif response_type == RESPONSE_TYPES.MOVE_PIECE_TO:
             response_choice, success, updated_board_str = move_piece_to.handle(
+                session_id, intent_data, board_str)
+        elif response_type == RESPONSE_TYPES.BEST_MOVE:
+            response_choice, success = best_move.handle(
                 session_id, intent_data, board_str)
 
     # Intents to handle after a game has finished
