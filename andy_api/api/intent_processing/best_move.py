0eac390a5a0439f25be0df077810a9f1af39b4e0
Nathaniel Stull <47871658+Nate-Stull@users.noreply.github.com>
2021-11-06 21:28:54 -0400

    Fixed and tested best_move.py

--- andy_api/api/intent_processing/best_move.py
+++ andy_api/api/intent_processing/best_move.py
@@ -2,25 +2,24 @@
 TODO add info about intent
 """
 
-from .utils import get_random_choice
+from utils import get_random_choice
 # TODO add api state manager variables for this interaction?
 
 import chess
+import chess.engine
 
 import os
 
 
 HAPPY_PATH_RESPONSES = [
-    "Lemme give ya a hand...Move {from_location} to {to_location}."
+    "Lemme give ya a hand... Move {from_location} to {to_location}.",
     "Okay! I know I'm pretty good, so here's a tip- move {from_location} to {to_location}."
 ]
-
-# TODO add error responses?
 
 
 def get_engine():
     dirname = os.path.dirname(__file__)
-    engine_filename = dirname + "../UCI_engine/stockfish"
+    engine_filename = dirname + "/../UCI_engine/stockfish"
     engine = chess.engine.SimpleEngine.popen_uci(engine_filename) #load stockfish as chess engine
     return engine
 
@@ -33,19 +32,18 @@ def get_best_move(board_str):
 
 def handle(session_id, intent_model, board_str):
     """TODO add details about method
-
     """
     if intent_model.all_required_params_present is True:
         static_choice = get_random_choice(HAPPY_PATH_RESPONSES)
-        best_move = get_best_move(board_str)
+        best_move = get_best_move(board_str).uci()
 
-        from_location = move[0:2]
-        to_location = move[2:4]
+        from_location = best_move[0:2]
+        to_location = best_move[2:4]
 
         return static_choice.format(
-            from_location=from_location,
+            from_location = from_location,
             to_location = to_location
-        ), True, board_str
+        ), True
 
         # TODO add error messages/check for errors from player?
 
