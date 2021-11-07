46105f0e179d72ebc38271f8a0dd8f2ce9a021f1
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2021-11-07 13:31:46 -0500

    fix: fixed best move

--- andy_api/api/intent_processing/best_move.py
+++ andy_api/api/intent_processing/best_move.py
@@ -5,6 +5,7 @@ import traceback
 from api.logging import log_error, ERROR_TYPES
 from api.chess_logic import get_best_move, get_piece_name_at
 from .utils import get_random_choice
+from api.state_manager import set_fulfillment_params
 
 HAPPY_PATH_RESPONSES = [
     "According to my calculations, you should move your {piece_name} from {from_location} to {to_location}.",
@@ -23,11 +24,17 @@ def handle(session_id, board_str):
     """
     static_choice = get_random_choice(HAPPY_PATH_RESPONSES)
     try:
-        best_move = get_best_move(board_str).uci()
+        best_move = get_best_move(board_str)
 
         from_location = best_move[0:2]
         to_location = best_move[2:4]
         piece_name = get_piece_name_at(board_str, from_location)
+
+        set_fulfillment_params(session_id, params={
+            "from_location": from_location,
+            "to_location": to_location,
+            "piece_name": piece_name,
+        })
 
         return static_choice.format(
             piece_name=piece_name,
